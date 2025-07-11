5e0d54bf8dfaa41a3294d2b18f09e949495c67c7
Admrzli. <adamrazali07@gmail.com>
2025-07-11 20:49:11 +0800

    Submission #1

--- Housing-Price/main.py
+++ Housing-Price/main.py
@@ -11,15 +11,20 @@ from torch import nn
 from torch import optim
 
 pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
 
 # loading the dataset
 
 train_data  = pd.read_csv('datasets/train.csv')
 test_data  = pd.read_csv('datasets/test.csv')
 
+print(len(train_data.values) * 0.01)
+
+id = train_data['Id']
+
 # Dropping ambiguous features
 
-train_data.drop(['MiscFeature', 'MiscVal', 'Id', 'PoolQC', 'Fence', 'Alley'], inplace=True, axis=1)
+train_data.drop(['MiscFeature', 'MiscVal', 'Id', 'PoolQC', 'Fence', 'Alley', 'Utilities', 'Condition2', 'HouseStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'Heating', 'Electrical', 'GarageQual'], inplace=True, axis=1)
 test_data.drop(['MiscFeature', 'MiscVal', 'Id', 'PoolQC', 'Fence', 'Alley'], inplace=True, axis=1)
 
 # Categorizing the features
@@ -30,22 +35,15 @@ combine_features = categorical_features + numerical_features
 
 # Checking the number of unique values per feature
 
-for col in categorical_features:
-    print(f"\nValue counts for '{col}':")
-    print(train_data[col].value_counts())
-
-
 # Checking and handling missing values
 
 categorical_imputer = SimpleImputer(strategy = 'most_frequent')
 numerical_imputer = SimpleImputer(strategy = 'mean')
 
 train_data[categorical_features] = categorical_imputer.fit_transform(train_data[categorical_features])
-
 train_data[numerical_features] = numerical_imputer.fit_transform(train_data[numerical_features])
 
 test_data[categorical_features] = categorical_imputer.fit_transform(test_data[categorical_features])
-
 test_data[[col for col in numerical_features if col != 'SalePrice']] = numerical_imputer.fit_transform(test_data[[col for col in numerical_features if col != 'SalePrice']])
 
 print(test_data.isnull().sum())
@@ -57,23 +55,33 @@ encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
 encoded_categorical = encoder.fit_transform(train_data[categorical_features])
 encoded_categorical = pd.DataFrame(encoded_categorical, columns=encoder.get_feature_names_out(categorical_features))
 
+encoded_categorical_test = encoder.fit_transform(test_data[categorical_features])
+encoded_categorical_test = pd.DataFrame(encoded_categorical_test, columns=encoder.get_feature_names_out(categorical_features))
+
+new_numerical_features = [col for col in numerical_features if col != 'SalePrice']
 new_categorical_features = encoded_categorical.columns.to_list()
 new_combined_features = new_categorical_features + numerical_features
+new_combined_features_test = new_categorical_features + new_numerical_features
+
 
 # Scaling numerical features
 
 scaler = StandardScaler()
 scaled_numerical = scaler.fit_transform(train_data[[col for col in numerical_features if col != 'SalePrice']])
 scaled_numerical = pd.DataFrame(scaled_numerical, columns=[col for col in numerical_features if col != 'SalePrice'])
+scaled_numerical_test = scaled_numerical.copy()
 scaled_numerical['SalePrice'] = train_data['SalePrice']
 
 # combining encoded and scaled features
 
 combined_data = pd.concat([encoded_categorical, scaled_numerical], axis=1)
-print('This is combined data', combined_data.shape)
+print('This is combined data', combined_data.columns)
+
+combined_data_test = pd.concat([encoded_categorical_test, scaled_numerical_test], axis = 1)
+print('This is combined data for testing', combined_data_test.columns)
 
 # Storing cleaned data into a csv file
-
+# combined_data_test.to_csv('cleaned_test.csv')
 # combined_data.to_csv('cleaned_train_data.csv')
 
 # Feature Selection via - ANOVA F-test
@@ -81,7 +89,9 @@ print('This is combined data', combined_data.shape)
 X = combined_data[[col for col in new_combined_features if col != 'SalePrice']]
 y = combined_data['SalePrice']
 
-selector = SelectKBest(score_func=f_classif, k=10)
+X_test = combined_data_test[[col for col in new_combined_features_test if col != 'SalePrice']]
+ 
+selector = SelectKBest(score_func=f_classif, k=274)
 selected = selector.fit_transform(X, y)
 selected_features = X.columns[selector.get_support()].to_list()
 
@@ -90,6 +100,8 @@ selected_features = X.columns[selector.get_support()].to_list()
 X = combined_data[selected_features].values
 X = torch.tensor(X, dtype = torch.float32)
 y = torch.tensor(y.values, dtype = torch.float32)
+
+X_test = torch.tensor(X_test.values, dtype=torch.float32)
 
 
 print('this is X',X)
@@ -135,7 +147,7 @@ y = y.to(device)
 
 # training phase
 
-num_epochs = 3000
+num_epochs = 740
 
 for epoch in range(num_epochs):
     model.train()
@@ -154,4 +166,15 @@ for epoch in range(num_epochs):
         model.eval()
         print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {loss_fn.item():.4f}")
 
+# Predicting test set
 
+model.eval()
+predictions = model(X_test).detach().numpy()
+
+submission = pd.DataFrame({
+    'Id': range(1461, 1461 + len(test_data)),
+    'SalePrice' : predictions.flatten()
+})
+submission.drop(submission.tail(1).index, inplace = True)
+
+submission.to_csv('submission.csv', index=False)
